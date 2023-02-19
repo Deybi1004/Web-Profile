f43a1d914d325f5d81ef9977d7094a5602877333
Deybi1004 <deybiorrego@gmail.com>
2023-02-19 03:34:39 -0500

    Component Header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">
+          Deybi
+        </a>
+
+        <div className="nav__menu">
+          <ul className="nal__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link">
+                <i className="uil uil-estate nav__icon"></i> Home
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav__icon"></i> About
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-file nav__icon"></i> Skills
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                <i className="uil uil-briefcase-alt nav__icon"></i> Services
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#portfolio" className="nav__link">
+                <i className="uil uil- nav__icon"></i> Portfolio
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-message nav__icon"></i> Contact
+              </a>
+            </li>
+          </ul>
+          <i className="uil uil-times nav__close"></i>
+        </div>
+        <div className="nav__toggle">
+            <i className="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
