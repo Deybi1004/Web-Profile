e6fffaaa2f11237bf49497e5e6a112008f38f79f
Deybi1004 <deybiorrego@gmail.com>
2023-03-02 23:25:12 -0500

    Skills /Component and CSS

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -2,7 +2,64 @@ import React from 'react'
 
 function Backend() {
   return (
-    <div>Backend</div>
+    <div className="skills__content">
+        <h3 className="skills__title">Backend Developer</h3>
+
+        <div className="skills__box">
+            <div className="skills__group">
+              
+                <div className="skills__data">
+                <i class='bx bx-badge-check'></i>
+                
+                <div>
+                  <h3 className="skills__name">PostgresSQL</h3>
+                  
+                </div>
+                </div>
+
+                <div className="skills__data">
+                <i class='bx bx-badge-check'></i>
+                
+                <div>
+                  <h3 className="skills__name">NodeJs</h3>
+                  
+                </div>
+                </div>
+
+                <div className="skills__data">
+                <i class='bx bx-badge-check'></i>
+                
+                <div>
+                  <h3 className="skills__name">ExpressJS</h3>
+                  
+                </div>
+                </div>
+            </div>
+
+            <div className="skills__group">
+              
+                <div className="skills__data">
+                <i class='bx bx-badge-check'></i>
+                
+                <div>
+                  <h3 className="skills__name">Sequelize</h3>
+                 
+                </div>
+                </div>
+
+                <div className="skills__data">
+                <i class='bx bx-badge-check'></i>
+                
+                <div>
+                  <h3 className="skills__name">Postman</h3>
+                  
+                </div>
+                </div>
+
+             
+            </div>
+        </div>
+    </div>
   )
 }
 
