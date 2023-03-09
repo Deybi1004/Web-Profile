573187cfd0c71ae9f30780794e3b2a6ccea70254
Deybi1004 <deybiorrego@gmail.com>
2023-03-08 23:20:12 -0500

    Component Qualification/jsx

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
+import Qualification from './components/qualification/Qualification';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <About/>
       <Skills/>
       <Services/>
+      <Qualification/>
     </main>
     </>
   );
