ece32ff0d050c22f2e428ea16214219d046b1f74
Deybi1004 <deybiorrego@gmail.com>
2023-03-13 17:06:58 -0500

    Component Testimonials and Data

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,27 @@
+import Image1 from "../../assets/testimonial1.png"
+import Image2 from "../../assets/testimonial2.png"
+import Image3 from "../../assets/testimonial3.png"
+
+export const Data = [
+    {
+        id:1,
+        image:Image1,
+        title: "Name 1",
+        description: 
+        'Lorem ipsum dolor sit amet, consect Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet'
+    },
+    {
+        id:2,
+        image:Image2,
+        title: "Name 2",
+        description: 
+        'Lorem ipsum dolor sit amet, consect Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet'
+    },
+    {
+        id:3,
+        image:Image3,
+        title: "Name 3",
+        description: 
+        'Lorem ipsum dolor sit amet, consect Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet'
+    }
+]
