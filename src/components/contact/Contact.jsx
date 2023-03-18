de4291c82179f957694fbecf0cbefd3be71a5112
Deybi1004 <deybiorrego@gmail.com>
2023-03-18 03:18:46 -0500

    Css/Form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -82,7 +82,7 @@ function Contact() {
               />
             </div>
 
-            <div className="contact__form-div">
+            <div className="contact__form-div contact__form-area">
               <label className="contact__form-tag">Project</label>
               <textarea
                 name="project"
