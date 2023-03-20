1edfcb2447380c58f2da5de98df54f1bcff5fbb0
Deybi1004 <deybiorrego@gmail.com>
2023-03-20 17:19:37 -0500

    Data for Component Work}

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 import ScrollUp from './components/scrollup/ScrollUp';
+import Work from './components/work/Work';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
       <Skills/>
       <Services/>
       <Qualification/>
+      <Work/>
       <Testimonials/>
       <Contact/>
       <Footer/>
